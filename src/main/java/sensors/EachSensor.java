32388a50a5e23e4d5d353be87949f8f5e1828b51
Grigorii Nizovoi <info@singulight.ru>
2016-01-02 19:34:48 +0300

    Add some getters / setters, add sensorId field

--- src/main/java/sensors/EachSensor.java
+++ src/main/java/sensors/EachSensor.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 /**
  * Created by Grigorii Nizovoi info@singulight.ru on 17.12.15.
+ * @version 0.1
+ *
+ *
  */
 public class EachSensor {
+
 
     public enum SensorType {
         TEMPERATURE,
@@ -28,16 +32,20 @@ public class EachSensor {
     }
 
     // Sensor description and state
+    private long sensorId;
     private String name;
     private String mqttPrefix;
     private SensorType type = SensorType.VOLTAGE;
-    private float value;                //Value if no TEXT type sensor
+    private float value = 0.0f;                //Value if no TEXT type sensor
     private float minValue = 0.0f;
     private float maxValue = 100.0f;
     private String textValue = "";      //Value if TEXT type sensor
     private List<ISensorAction> sensorActions = new LinkedList<>();
 
-
+    /**
+     * Run all actions. Each sensor has no, one or many actions. Each action acts on the one actuator.
+     * This method runs all actions attached to this sensor.
+     */
     public void actionsGo() {
         sensorActions.forEach((action) -> {
             if (type == SensorType.TEXT) {
@@ -47,4 +55,56 @@ public class EachSensor {
             }
         });
     }
+
+/*Getters and Setters */
+
+    public long getSensorId() {
+        return sensorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMqttPrefix() {
+        return mqttPrefix;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public float getMinValue() {
+        return minValue;
+    }
+
+    public float getMaxValue() {
+        return maxValue;
+    }
+
+    public String getTextValue() {
+        return textValue;
+    }
+
+    public void configure(long sensorId,String name,String mqttPrefix,SensorType type,float minValue,float maxValue) {
+        this.sensorId = sensorId;
+        this.name = name;
+        this.mqttPrefix = mqttPrefix;
+        this.type = type;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    public void addAction(ISensorAction action) {
+        this.sensorActions.add(action);
+    }
+
+    public void removeAction(ISensorAction action) {
+        sensorActions.remove(action);
+    }
+
+    public List<ISensorAction> getSensorActions() {
+        return this.sensorActions;
+    }
+
 }
