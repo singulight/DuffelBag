493d29d03f2f3cd24e12e990951efcc2a2804f2c
Grigorii Nizovoi <info@singulight.ru>
2016-01-18 20:26:29 +0300

    Some

--- src/main/test/ru/singulight/duffelbag/mqttnodes/SensorNodeTest.java
+++ src/main/test/ru/singulight/duffelbag/mqttnodes/SensorNodeTest.java
@@ -3,7 +3,13 @@ package ru.singulight.duffelbag.mqttnodes;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ru.singulight.duffelbag.mqttnodes.actions.ISensorAction;
+import ru.singulight.duffelbag.mqttnodes.actions.LuaScriptSensorAction;
+import ru.singulight.duffelbag.mqttnodes.actions.SetOfRulesSensorAction;
 import ru.singulight.duffelbag.mqttnodes.types.SensorType;
+
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,7 +23,7 @@ public class SensorNodeTest {
     @Before
     public void setUp() throws Exception {
 
-        sensorNode = new SensorNode(0x100,"Out temperature","duffelbag/trmperature/0000000000000100", SensorType.OTHER,0,100);
+        sensorNode = new SensorNode(0x100,"Out temperature","duffelbag/temperature/0000000000000100", SensorType.OTHER,0,100);
 
     }
 
@@ -28,5 +34,34 @@ public class SensorNodeTest {
     @Test
     public void TestCreateObject() {
         assertTrue("object not create",sensorNode != null);
+        assertEquals(sensorNode.getId(),0x100);
+        assertEquals(sensorNode.getName(),"Out temperature");
+        assertEquals(sensorNode.getMqttTopic(),"duffelbag/temperature/0000000000000100");
+        assertEquals(sensorNode.isKnown(),false);
+        assertEquals(sensorNode.getSensorType(),SensorType.OTHER);
+        assertEquals(sensorNode.getMinValue(),0,0);
+        assertEquals(sensorNode.getMaxValue(),100,0);
+    }
+
+    @Test
+    public void TestActions() {
+        ISensorAction action1 = new LuaScriptSensorAction();
+        ISensorAction action2 = new LuaScriptSensorAction();
+        ISensorAction action3 = new SetOfRulesSensorAction();
+        List<ISensorAction> ref = new LinkedList<>();
+        ref.add(action1);
+        ref.add(action2);
+        ref.add(action3);
+        sensorNode.addAction(action1);
+        sensorNode.addAction(action2);
+        sensorNode.addAction(action3);
+        assertEquals(sensorNode.getSensorActions(),ref);
+        ref.remove(action1);
+        ref.remove(action2);
+        ref.remove(action3);
+        sensorNode.removeAction(action1);
+        sensorNode.removeAction(action2);
+        sensorNode.removeAction(action3);
+        assertEquals(sensorNode.getSensorActions(),ref);
     }
 }
