065d2d45c2c8ff978cdce9eb2c2a7504557d3f9a
Grigorii Nizovoi <info@singulight.ru>
2016-08-19 00:23:29 +0300

    Refactor

--- src/main/java/ru/singulight/duffelbag/dao/NodeDao.java
+++ src/main/java/ru/singulight/duffelbag/dao/NodeDao.java
@@ -5,7 +5,7 @@ import ru.singulight.duffelbag.nodes.SensorNode;
 /**
  * Created by Grigorii Nizovoy info@singulight.ru on 11.07.16.
  */
-public interface SensorDao {
+public interface NodeDao {
     public void mergeAllSensors();
     public void saveAllSensors();
     public void getSensor(String topic);
--- src/main/java/ru/singulight/duffelbag/dao/NodeDaoH2.java
+++ src/main/java/ru/singulight/duffelbag/dao/NodeDaoH2.java
@@ -3,12 +3,10 @@ package ru.singulight.duffelbag.dao;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.singulight.duffelbag.nodes.SensorNode;
 
-import java.sql.SQLException;
-
 /**
  * Created by Grigorii Nizovoy info@singulight.ru on 21.07.16.
  */
-public class SensorDaoImpl implements SensorDao {
+public class NodeDaoH2 implements NodeDao {
 
 
     private JdbcTemplate jdbcTemplate;
